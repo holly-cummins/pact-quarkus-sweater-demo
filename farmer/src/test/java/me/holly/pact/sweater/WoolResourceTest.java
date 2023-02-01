45b11901c36b05966f47696fe5c96c1ab50e43cd
Holly Cummins <hcummins@redhat.com>
2023-02-01 14:32:13 +0000

    Change to using objects

--- farmer/src/test/java/me/holly/pact/sweater/WoolResourceTest.java
+++ farmer/src/test/java/me/holly/pact/sweater/WoolResourceTest.java
@@ -1,21 +1,36 @@
 package me.holly.pact.sweater;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 public class WoolResourceTest {
 
     @Test
-    public void testWoolEndpoint() {
-        given()
+    public void testWoolEndpointForWhiteWool() {
+        Skein skein = given()
                 .when().get("/wool/order/white")
                 .then()
                 .statusCode(200)
-                .body("colour", equalTo("white"));
+                .extract().as(Skein.class);
+
+        assertEquals(skein.getColour(), "white");
+    }
+
+    @Test
+    @Disabled
+    public void testWoolEndpointForBlackWool() {
+        Skein skein = given()
+                .when().get("/wool/order/black")
+                .then()
+                .statusCode(200)
+                .extract().as(Skein.class);
+
+        assertEquals(skein.getColour(), "black");
     }
 
 }
