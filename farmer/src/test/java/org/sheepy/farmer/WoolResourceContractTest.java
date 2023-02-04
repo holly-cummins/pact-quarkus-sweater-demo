359e86d0fdd8be1cf3c7942419752ea02d390f9d
Holly Cummins <hcummins@redhat.com>
2023-02-04 14:16:33 +0000

    Add provider contract test

---
+++ farmer/src/test/java/org/sheepy/farmer/WoolResourceContractTest.java
@@ -0,0 +1,32 @@
+package org.sheepy.farmer;
+
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import io.quarkus.test.junit.QuarkusTest;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@Provider("farmer")
+@PactFolder("pacts")
+@QuarkusTest // This starts the server for convenience in testing
+public class WoolResourceContractTest {
+
+    @ConfigProperty(name = "quarkus.http.test-port")
+    int quarkusPort;
+
+    @BeforeEach
+    void before(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", quarkusPort));
+    }
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+}
