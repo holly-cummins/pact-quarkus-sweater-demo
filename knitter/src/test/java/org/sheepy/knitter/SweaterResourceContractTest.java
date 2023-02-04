add390194322c72b25530ef9f43184f0b7502333
Holly Cummins <hcummins@redhat.com>
2023-02-04 13:16:28 +0000

    Add in basic contract test

---
+++ knitter/src/test/java/org/sheepy/knitter/SweaterResourceContractTest.java
@@ -0,0 +1,61 @@
+package org.sheepy.knitter;
+
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.core.model.V4Pact;
+import au.com.dius.pact.core.model.annotations.Pact;
+import au.com.dius.pact.core.model.annotations.PactDirectory;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import javax.ws.rs.HttpMethod;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "farmer", port = "8084")
+@PactDirectory("target/pacts")
+@QuarkusTest
+public class SweaterResourceContractTest {
+
+    @Pact(provider = "farmer", consumer = "knitter")
+    public V4Pact createPact(PactDslWithProvider builder) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        // Here we define our mock, which is also our expectations for the provider
+        String sweaterBody = "{\"colour\":\"white\"}\n";
+        return builder.given("test post")
+                .uponReceiving("post request")
+                .path("/wool/order")
+                .method(HttpMethod.POST)
+                .willRespondWith()
+                .status(200)
+                .headers(headers)
+                .body(sweaterBody)
+                .toPact(V4Pact.class);
+    }
+
+    @Test
+    public void testSweaterEndpointForWhiteSweater() {
+        SweaterOrder order = new SweaterOrder("white", 12);
+        Sweater sweater = given()
+                .contentType(ContentType.JSON)
+                .body(order)
+                .when()
+                .post("/sweater/order")
+                .then()
+                .statusCode(200)
+                .extract().as(Sweater.class);
+
+        assertEquals("white", sweater.getColour());
+    }
+
+
+}
--- knitter/src/test/java/org/sheepy/knitter/SweaterResourceTest.java
+++ knitter/src/test/java/org/sheepy/knitter/SweaterResourceTest.java
@@ -1,14 +1,28 @@
 package org.sheepy.knitter;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
 import io.restassured.http.ContentType;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @QuarkusTest
 public class SweaterResourceTest {
+
+    @InjectMock
+    @RestClient
+    FarmerService mock;
+
+    @BeforeEach
+    public void setUp() {
+        when(mock.getWool(any())).thenReturn(new Skein("white"));
+    }
 
     @Test
     public void testSweaterEndpointForWhiteSweater() {
