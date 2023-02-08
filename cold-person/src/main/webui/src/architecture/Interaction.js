dbb8c9685308a177fad0200933f6e49636c26076
Holly Cummins <hcummins@redhat.com>
2023-02-08 00:16:56 +0000

    Visualise exceptions

--- cold-person/src/main/webui/src/architecture/Interaction.js
+++ cold-person/src/main/webui/src/architecture/Interaction.js
@@ -31,7 +31,9 @@ const InteractionDisplay = styled.div`
     box-shadow: 20px 38px 34px -26px hsla(0, 0%, 0%, .2);
   }
 
-  border: solid 4px #41403E;
+  border-width: 4px;
+  border-style: solid;
+  border-color: ${props => props.isException ? "#993300" : "#41403E"};
 
 
 `
@@ -55,10 +57,14 @@ const Interaction = ({interaction}) => {
         setOpen(false)
     }
 
+    // The exception looks like json, but we haven't parsed it, so it's just a string
+    const isException = interaction.payload?.includes("exception");
+    console.log(isException, "got", interaction.payload, interaction.payload?.exception)
+
     return (
 
         <InteractionDisplay onMouseOver={handleOpen}
-                            onMouseOut={handleClose}>
+                            onMouseOut={handleClose} isException={isException}>
             {interaction.methodName}
             {isOpen && (<Payload>
                 {interaction.payload}
